2501a94ff61e383566bef19fde3d3db9557736d9
D-Luffy-36 <phaman472004@gmail.com>
2024-10-25 14:05:52 +0700

    first commit

---
+++ src/main/java/uth/edu/podbooking/PodbookingApplication.java
@@ -0,0 +1,13 @@
+package uth.edu.podbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodbookingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uth/edu/podbooking/common/ResponseObject.java
@@ -0,0 +1,7 @@
+package uth.edu.podbooking.common;
+
+public class ResponseObject {
+    private String status;
+    private String message;
+    private Object data;
+}
