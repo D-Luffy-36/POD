a7d6a6582e2b849a8009aebd4c4bbf0a34de264e
HungLit <2251120014@ut.edu.vn>
2024-11-20 17:38:18 +0700

    call Api

---
+++ src/main/resources/Template/admin/js/serviceApi.js
@@ -0,0 +1,7 @@
+// Load file sidebar.html
+fetch('../layout/sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('sidebar').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading sidebar:', error));
